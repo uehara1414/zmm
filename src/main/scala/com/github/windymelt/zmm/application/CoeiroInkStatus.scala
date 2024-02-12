32656ea6c754fb42801448acfb56218330c6e2dc
uehara1414 <b1015062@fun.ac.jp>
2024-02-12 19:17:04 +0900

    試行錯誤

--- src/main/scala/com/github/windymelt/zmm/application/CoeiroInkStatus.scala
+++ src/main/scala/com/github/windymelt/zmm/application/CoeiroInkStatus.scala
@@ -2,17 +2,25 @@ package com.github.windymelt.zmm.application
 
 import cats.effect.IO
 import com.github.windymelt.zmm.{domain, infrastructure}
-import com.github.windymelt.zmm.infrastructure.CoeiroInkComponent
 
 class CoeiroInkStatus extends infrastructure.CoeiroInkComponent
-    with domain.repository.CoeiroInkComponent:
+    with domain.repository.CoeiroInkComponent {
+  override def coeiroInk: CoeiroInk = new ConcreteCoeiroInk()
 
-  def coeiroInk = new ConcreteCoeiroInk()
-
-  def show(): IO[Unit] =
+  def show(): IO[Unit] = {
+    val speakerObject = coeiroInk.speakers
+    val speakers = speakerObject.map(_.asArray.getOrElse(Vector()))
     for {
-      _ <- IO.println("hogehoge in CoeiroInkStatus.show")
-      speakers <- coeiroInk.speakers
-      _ <- IO.println(s"speakers: $speakers")
-      _ <- IO.println("CoeiroInkStatus.show finish")
+      result <- speakers.flatMap {
+        vec => {
+          IO {
+            vec.map { json =>
+              json.asObject.flatMap(_("speakerName"))
+            }
+          }
+        }
+      }
+      _ <- IO.println(result.map(_.get))
     } yield ()
+  }
+}
--- src/main/scala/com/github/windymelt/zmm/infrastructure/CoeiroInk.scala
+++ src/main/scala/com/github/windymelt/zmm/infrastructure/CoeiroInk.scala
@@ -2,6 +2,13 @@ package com.github.windymelt.zmm
 package infrastructure
 
 import cats.effect.IO
+import io.circe.{Decoder, Json}
+import io.circe.generic.semiauto.deriveDecoder
+import org.http4s.{EntityDecoder, Request, Uri}
+import org.http4s.ember.client.EmberClientBuilder
+import org.http4s.circe.CirceEntityDecoder.*
+
+case class SpeakerResponse(speakerName: String)
 
 case class Speaker(
   name: String
@@ -9,14 +16,37 @@ case class Speaker(
 
 trait CoeiroInkComponent {
   self: domain.repository.CoeiroInkComponent =>
-  type Speakers = Seq[Speaker]
+  type Speakers = Json
 
   def coeiroInk: CoeiroInk
 
   class ConcreteCoeiroInk extends CoeiroInk:
-    def speakers: IO[Speakers] =
-      for {
-        _ <- IO.println("CoeiroInkComponent.ConcreteCoeiroInk.speakers")
-        speakers = Seq(Speaker("つくよみちゃん in local"), Speaker("黄琴海月 in local"))
-      } yield speakers
+    implicit def speakerDecoder: Decoder[List[SpeakerResponse]] = deriveDecoder[List[SpeakerResponse]]
+    implicit def responseDecoder:  EntityDecoder[IO, List[SpeakerResponse]] = circeEntityDecoder[IO, List[SpeakerResponse]]
+
+    def speakers: IO[Speakers] = {
+      client.use { c =>
+        val url = "http://localhost:50032/v1/speakers"
+        val req = Request[IO](uri = Uri.fromString(url).fold(throw _, identity))
+        val res = c.expect[Json](req)
+
+        for {
+          _ <- IO.println(url)
+          _ <- IO.println(res)
+          r <- res
+        } yield {
+          r
+        }
+      }
+    }
+
+    private lazy val client = {
+      import concurrent.duration._
+      import scala.language.postfixOps
+      EmberClientBuilder
+        .default[IO]
+        .withTimeout(5 minutes)
+        .withIdleConnectionTime(10 minutes)
+        .build
+    }
 }
