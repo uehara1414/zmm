96644a386399a1b035bab0885f62767325f64608
uehara1414 <b1015062@fun.ac.jp>
2024-02-12 21:41:50 +0900

    型を定義

--- src/main/scala/com/github/windymelt/zmm/application/CoeiroInkStatus.scala
+++ src/main/scala/com/github/windymelt/zmm/application/CoeiroInkStatus.scala
@@ -3,28 +3,24 @@ package com.github.windymelt.zmm.application
 import cats.effect.IO
 import com.github.windymelt.zmm.{domain, infrastructure, util}
 
-class CoeiroInkStatus extends infrastructure.CoeiroInkComponent
-  with domain.repository.CoeiroInkComponent
-  with util.UtilComponent {
+class CoeiroInkStatus
+    extends infrastructure.CoeiroInkComponent
+    with domain.repository.CoeiroInkComponent
+    with util.UtilComponent {
   override def coeiroInk: CoeiroInk = {
-    val uri = sys.env.get("COEIROINK_URI") getOrElse config.getString("coeiroink.apiUri")
+    val uri = sys.env.get("COEIROINK_URI") getOrElse config.getString(
+      "coeiroink.apiUri"
+    )
     new ConcreteCoeiroInk(uri)
   }
 
   def show(): IO[Unit] = {
-    val speakerObject = coeiroInk.speakers
-    val speakers = speakerObject.map(_.asArray.getOrElse(Vector()))
     for {
-      result <- speakers.flatMap {
-        vec => {
-          IO {
-            vec.map { json =>
-              json.asObject.flatMap(_("speakerName"))
-            }
-          }
+      _ <- coeiroInk.speakers.flatMap { speakers =>
+        IO {
+          println(s"Speakers: ${speakers.map(_.name).mkString(", ")}")
         }
       }
-      _ <- IO.println(result.map(_.get))
-    } yield ()
+    } yield {}
   }
 }
--- src/main/scala/com/github/windymelt/zmm/infrastructure/CoeiroInk.scala
+++ src/main/scala/com/github/windymelt/zmm/infrastructure/CoeiroInk.scala
@@ -16,27 +16,32 @@ case class Speaker(
 
 trait CoeiroInkComponent {
   self: domain.repository.CoeiroInkComponent =>
-  type Speakers = Json
+  type Speakers = Seq[Speaker]
 
   def coeiroInk: CoeiroInk
 
   class ConcreteCoeiroInk(var uri: String) extends CoeiroInk:
-    implicit def speakerDecoder: Decoder[List[SpeakerResponse]] = deriveDecoder[List[SpeakerResponse]]
-    implicit def responseDecoder:  EntityDecoder[IO, List[SpeakerResponse]] = circeEntityDecoder[IO, List[SpeakerResponse]]
-
     def speakers: IO[Speakers] = {
       client.use { c =>
         val url = s"$uri/v1/speakers"
         val req = Request[IO](uri = Uri.fromString(url).fold(throw _, identity))
         val res = c.expect[Json](req)
+        val response = res.map(_.asArray.getOrElse(Vector()))
 
         for {
-          _ <- IO.println(url)
-          _ <- IO.println(res)
-          r <- res
-        } yield {
-          r
-        }
+          result <- response.flatMap {
+            vec => {
+              IO {
+                vec.map {
+                  json =>
+                    json
+                      .asObject
+                      .flatMap(_("speakerName"))
+                      .flatMap(_.asString)}
+              }
+            }
+          }
+        } yield result.flatMap(_.toSeq).map(Speaker(_))
       }
     }
 
