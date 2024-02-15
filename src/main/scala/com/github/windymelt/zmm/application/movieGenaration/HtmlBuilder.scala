f7bad3a2fb7ec1cded6d1911c0347194720b8e38
uehara1414 <b1015062@fun.ac.jp>
2024-02-15 23:37:29 +0900

    デバッグ情報を動画中に表示できるよう変更

--- src/main/scala/com/github/windymelt/zmm/application/movieGenaration/HtmlBuilder.scala
+++ src/main/scala/com/github/windymelt/zmm/application/movieGenaration/HtmlBuilder.scala
@@ -5,9 +5,9 @@ import com.github.windymelt.zmm.domain.model.Context
 
 
 class HtmlBuilder {
-  def build(serif: String, ctx: Context): IO[String] = {
+  def build(serif: String, ctx: Context, debuggingInfo: Seq[String] = Seq("文字列を動画中に表示します", "デバッグに使いましょう")): IO[String] = {
     IO {
-      html.sample(serif = serif, ctx = ctx).body
+      html.sample(serif = serif, ctx = ctx, debuggingInfo = debuggingInfo).body
     }
   }
 }
