ccde223e08a1e1846e582a40adc03756df4f07c5
uehara1414 <b1015062@fun.ac.jp>
2024-02-16 13:33:13 +0900

    固定Urlの立ち絵のバリエーションを出力してみる

--- src/main/scala/com/github/windymelt/zmm/application/TachieStatus.scala
+++ src/main/scala/com/github/windymelt/zmm/application/TachieStatus.scala
@@ -3,13 +3,23 @@ package com.github.windymelt.zmm.application
 import cats.effect.IO
 
 class TachieStatus {
+  val tachieUrl = "../../assets/黄琴海月.png"
 
   /** 立ち絵関連の状態を表示する
     * @return
     *   IO[Unit]
     */
   def execute(): IO[Unit] = {
-    IO.print(s"WIP 立ち絵を表示するよ") >>
-      IO.println("")
+    val ExtRe = """(.+)\.(.+)""".r.anchored
+
+    val alternate = tachieUrl match {
+      case originalPath @ ExtRe(file: String, ext: String) =>
+        s"${file}!.$ext"
+      case _ => ???
+    }
+
+    IO.println(s"WIP 立ち絵を表示するよ") >>
+      IO.println(s"${tachieUrl}") >>
+      IO.println(s"${alternate}")
   }
 }
